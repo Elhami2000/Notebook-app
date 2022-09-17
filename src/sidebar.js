2212a64c83efc2375ee9cc6767bed6d0a65102d7
Elhami2000 <73657625+Elhami2000@users.noreply.github.com>
2022-09-17 20:36:12 +0200

    Added sortedNotes function

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,11 +1,12 @@
 function Sidebar({ notes, onAddNote, onDeleteNote, activeNote, setActiveNote }) {
+    const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
     return <div className="app-sidebar">
         <div className="app-sidebar-header">
             <h1>Notes</h1>
             <button onClick={onAddNote}>Add</button>
         </div>
         <div className="app-sidebar-notes">
-            {notes.map((note) => (
+            {sortedNotes.map((note) => (
                 <div className={`app-sidebar-note ${note.id === activeNote && "active"}`} onClick={() => setActiveNote(note.id)}>
                     <div className="sidebar-note-title">
                         <strong>{note.title}</strong>
