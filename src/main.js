214fa014a2db0484d186b9cf88dfa4c2c1fae8b8
Elhami2000 <73657625+Elhami2000@users.noreply.github.com>
2022-09-19 14:58:39 +0200

    Added a simple category function.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 function Main({ activeNote, onUpdateNote }) {
     const onEditField = (key, value) => {
         onUpdateNote({
@@ -6,8 +7,16 @@ function Main({ activeNote, onUpdateNote }) {
             lastModified: Date.now()
         });
     };
+    const getInitialState = () => {
+        const value = "Category";
+        return value;
+    };
+    const [value, setValue] = useState(getInitialState);
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
 
-    if(!activeNote){
+    if (!activeNote) {
         return <div className="no-active-note">Notes have not been selected.</div>
     }
 
@@ -15,15 +24,21 @@ function Main({ activeNote, onUpdateNote }) {
         <div className="app-main-note-preview">
             <h1 className="preview-title">{activeNote.title}</h1>
             <h4 className="preview-title">
-                       Category -  Date {new Date(activeNote.lastModified).toLocaleDateString("en-GB", {hour: "2-digit", minute: "2-digit"})}
-                    </h4>
+            <select style={{marginRight: "10px"}} value={value} onChange={handleChange}>
+                <option value="Category">Category</option>
+                <option value="Reminder">Reminder</option>
+                <option value="Note">Note</option>
+                <option value="ToDo">To do</option>
+            </select>
+                {`${value}`} -  Date {new Date(activeNote.lastModified).toLocaleDateString("en-GB", { hour: "2-digit", minute: "2-digit" })}
+            </h4>
             <div className="markdown-preview">{activeNote.body}</div>
         </div>
         <div className="app-main-note-edit">
             <input type="text" id="title" value={activeNote.title} onChange={(e) => onEditField("title", e.target.value)} autoFocus />
             <textarea id="body" placeholder="Type a note!" value={activeNote.body} onChange={(e) => onEditField("body", e.target.value)} />
         </div>
-        
+
     </div>;
 }
 
